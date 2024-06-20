d351a6a33c56136c473c6728427cdec4f606d413
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-06-20 16:06:45 +0200

    fix: locales

---
+++ src/overrideTranslations.jsx
@@ -0,0 +1,71 @@
+import { defineMessages } from 'react-intl';
+
+defineMessages({
+  rrule_st: {
+    id: 'rrule_st',
+    defaultMessage: '_', //in italiano non si traduce
+  },
+  rrule_nd: {
+    id: 'rrule_nd',
+    defaultMessage: '_', //in italiano non si traduce
+  },
+  rrule_rd: {
+    id: 'rrule_rd',
+    defaultMessage: '_', //in italiano non si traduce
+  },
+  rrule_th: {
+    id: 'rrule_th',
+    defaultMessage: '_', //in italiano non si traduce
+  },
+  rrule_in: {
+    id: 'rrule_in',
+    defaultMessage: 'il giorno',
+  },
+  rrule_on: {
+    id: 'rrule_on',
+    defaultMessage: 'di',
+  },
+  rrule_on_the: {
+    id: 'rrule_on the',
+    defaultMessage: 'di',
+  },
+  weekdays: {
+    id: 'rrule_weekdays',
+    defaultMessage: 'giorni feriali',
+  },
+  weekday: {
+    id: 'rrule_weekday',
+    defaultMessage: 'giorno feriale',
+  },
+  reset: {
+    //volto-form-block
+    id: 'form_reset',
+    defaultMessage: 'Ricomincia',
+  },
+  legend_required: {
+    //volto-form-block
+    id: 'legend_required',
+    defaultMessage: 'I campi contrassegnati da (*) sono obbligatori.',
+  },
+  default_cancel_label: {
+    //volto-form-block
+    id: 'form_default_cancel_label',
+    defaultMessage: 'Annulla',
+  },
+  toggleMenu: {
+    id: 'toggle-menu',
+    defaultMessage: '{action} il menu',
+  },
+  toggleMenu_open: {
+    id: 'toggleMenu_open',
+    defaultMessage: 'Apri',
+  },
+  toggleMenu_close: {
+    id: 'toggleMenu_close',
+    defaultMessage: 'Chiudi',
+  },
+  clearDates: {
+    id: 'Clear dates',
+    defaultMessage: 'Pulisci i campi',
+  },
+});
