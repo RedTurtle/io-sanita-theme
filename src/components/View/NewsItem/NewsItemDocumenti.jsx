6160798aed322510f317421a2d6e4ae3ecdad308
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-09-09 10:32:19 +0200

    fix: news_item view folders

--- src/components/View/NewsItem/NewsItemDocumenti.jsx
+++ src/components/View/NewsItem/NewsItemDocumenti.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { defineMessages, useIntl } from 'react-intl';
 
 import { Attachments } from 'io-sanita-theme/components/View/commons';
 
+const messages = defineMessages({
+  documenti: {
+    id: 'news_documenti',
+    defaultMessage: 'Documenti',
+  },
+});
+
 const NewsItemDocumenti = ({ content }) => {
-  return <Attachments content={content} folder_name={'documenti-allegati'} />;
+  const intl = useIntl();
+
+  return (
+    <Attachments
+      content={content}
+      folder_name={'documenti'}
+      title={intl.formatMessage(messages.documenti)}
+    />
+  );
 };
 
 NewsItemDocumenti.propTypes = {
