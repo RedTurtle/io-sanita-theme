e362ce35caa214c701f6c4c252081af53f70f4f2
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-09-11 14:57:17 +0200

    fix: skeleton styles for inevidence template

--- src/components/Blocks/Listing/Skeletons/InEvidenceTemplateSkeleton.jsx
+++ src/components/Blocks/Listing/Skeletons/InEvidenceTemplateSkeleton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import {
   Container,
   Card,
@@ -7,8 +8,9 @@ import {
   CardTitle,
   CardText,
   Chip,
+  Row,
+  Col,
 } from 'design-react-kit';
-import cx from 'classnames';
 
 const InEvidenceTemplateSkeleton = ({
   title,
@@ -21,36 +23,51 @@ const InEvidenceTemplateSkeleton = ({
       <Container className="px-4">
         <div className="skeleton-template">
           <div className="in-evidence-cards-wrapper mb-5">
-            {[0, 1, 2, 3, 4].map((i) => {
-              return (
-                <Card key={i} className={cx('listing-item')}>
-                  {i === 0 && (
-                    <div className="img-responsive-wrapper">
-                      <div className="img-responsive">
-                        <figure className="img-wrapper"></figure>
-                      </div>
-                    </div>
-                  )}
+            <Row>
+              {[0, 1, 2, 3, 4].map((i) => {
+                return (
+                  <Col lg={i > 0 ? 6 : 12}>
+                    <Card
+                      teaser
+                      key={i}
+                      className={cx(
+                        'shadow rounded card-teaser-image card-flex',
+                      )}
+                      wrapperClassName={cx(
+                        'card-teaser-wrapper-equal card-teaser-block-2 card-featured',
+                        {
+                          'card-featured-small': i > 0,
+                          'card-featured-large': i === 0,
+                        },
+                      )}
+                    >
+                      <CardBody className="p-4">
+                        <CardTitle tag="h2"> </CardTitle>
+                        <CardText className="mb-3"> </CardText>
+                        <div>
+                          {[0, 1]?.map((index) => (
+                            <Chip
+                              color="primary"
+                              disabled={false}
+                              simple
+                              tag="div"
+                              className="me-2"
+                              key={index}
+                            ></Chip>
+                          ))}
+                        </div>
+                      </CardBody>
 
-                  <CardBody className="px-4">
-                    <CardTitle tag="h4"> </CardTitle>
-                    <CardText className="mb-3"> </CardText>
-                    <div>
-                      {[0, 1]?.map((index) => (
-                        <Chip
-                          color="primary"
-                          disabled={false}
-                          simple
-                          tag="div"
-                          className="me-2"
-                          key={index}
-                        ></Chip>
-                      ))}
-                    </div>
-                  </CardBody>
-                </Card>
-              );
-            })}
+                      <div className="card-image card-image-rounded">
+                        <div className="img-responsive">
+                          <div className="img-wrapper"></div>
+                        </div>
+                      </div>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
           </div>
           {linkHref && <div className="link-button text-center"></div>}
         </div>
