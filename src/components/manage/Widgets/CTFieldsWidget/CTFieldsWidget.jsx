768e97401ac15782d707ab6269aa2159788cd693
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-03-21 14:06:10 +0100

    fix: widget campo

--- src/components/manage/Widgets/CTFieldsWidget/CTFieldsWidget.jsx
+++ src/components/manage/Widgets/CTFieldsWidget/CTFieldsWidget.jsx
@@ -2,38 +2,83 @@ import React, { useEffect } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCTSchema } from 'io-sanita-theme/actions';
-import { SelectAutoComplete } from '@plone/volto/components/manage/Widgets';
+import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
+import FormFieldWrapper from '@plone/volto/components/manage/Widgets/FormFieldWrapper';
+import { normalizeValue } from '@plone/volto/components/manage/Widgets/SelectUtils';
 
-const CTFieldsWidget = ({ ct, ...props }) => {
+import {
+  Option,
+  DropdownIndicator,
+  selectTheme,
+  customSelectStyles,
+} from '@plone/volto/components/manage/Widgets/SelectStyling';
+
+const CTFieldsWidget = ({ ct, reactSelect, ...props }) => {
   const intl = useIntl();
   const dispatch = useDispatch();
-  const ct_fields = useSelector(
-    (state) => state.ct_schema?.subrequests?.[ct]?.result?.properties,
+  const Select = reactSelect.default;
+  const exclude_fieldsets = ['ownership', 'seo'];
+  const ct_schema = useSelector(
+    (state) => state.ct_schema?.subrequests?.[ct]?.result,
   );
 
   useEffect(() => {
-    if (!ct_fields) {
+    if (!ct_schema) {
       dispatch(getCTSchema(ct));
     }
   }, [ct]);
 
-  const choices = ct_fields
-    ? Object.keys(ct_fields).map((f) => {
-        return [f, ct_fields[f].title];
-      })
-    : [];
-  console.log('ct_fields', ct, ct_fields, choices);
-  return (
-    <>
-      {ct_fields && (
-        <SelectAutoComplete
-          {...props}
-          choices={choices}
-          // fieldSet="default"
-        />
-      )}
-    </>
+  const groups_choices =
+    ct_schema?.fieldsets
+      ?.filter((fieldset) => exclude_fieldsets.indexOf(fieldset.id) < 0)
+      .map((fieldset) => {
+        return {
+          label: fieldset.title,
+          options: fieldset.fields.map((f) => {
+            return {
+              value: f,
+              label: ct_schema.properties[f].title,
+            };
+          }),
+        };
+      }) ?? [];
+
+  const disabled = props.disabled || props.isDisabled;
+  const normalizedValue = normalizeValue(
+    groups_choices.reduce((acc, fieldset) => {
+      return [...acc, ...fieldset.options];
+    }, []),
+    props.value,
+    intl,
+  );
+
+  return groups_choices?.length > 0 ? (
+    <FormFieldWrapper {...props}>
+      <Select
+        id={`field-${props.id}`}
+        name={props.id}
+        disabled={disabled}
+        className="react-select-container"
+        classNamePrefix="react-select"
+        placeholder={props.placeholder ?? 'Seleziona un valore'}
+        options={groups_choices}
+        styles={customSelectStyles}
+        theme={selectTheme}
+        components={{ DropdownIndicator, Option }}
+        value={normalizedValue}
+        onChange={(selectedOption) => {
+          props.onChange(
+            props.id,
+            selectedOption && selectedOption.value !== 'no-value'
+              ? selectedOption.value
+              : undefined,
+          );
+        }}
+      />
+    </FormFieldWrapper>
+  ) : (
+    <></>
   );
 };
 
-export default CTFieldsWidget;
+export default injectLazyLibs('reactSelect')(CTFieldsWidget);
