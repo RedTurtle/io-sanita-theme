deebe14403c079136e97acbfee5a73dedcad9531
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-03-25 11:42:50 +0100

    chore: added margin bottom to accordion block full-width

--- src/components/Blocks/Accordion/Container.jsx
+++ src/components/Blocks/Accordion/Container.jsx
@@ -5,7 +5,7 @@ import { TextBlockView } from '@plone/volto-slate/blocks/Text';
 const AccordionContainer = ({ data, headerChildren, children }) => {
   return data.show_block_bg ? (
     <div className="public-ui">
-      <div className="full-width section section-muted section-inset-shadow py-5">
+      <div className="full-width section section-muted section-inset-shadow py-5 mb-4">
         <Container className="px-md-4">
           <Card className="card-bg rounded no-after" noWrapper={false}>
             {(data.title || data.description || headerChildren) && (
