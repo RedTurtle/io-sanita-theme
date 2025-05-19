073148a9987b6d2cb045c3a0c274e911ca9a7258
Sara Bianchi <43245702+SaraBianchi@users.noreply.github.com>
2025-05-19 13:44:44 +0200

    feat: Schema.org Event and Homepage (#87)
    * feat: Schema.org Event and Homepage
    
    * fix: fixed url and description params

--- src/components/Home/HomeSchemaOrg.jsx
+++ src/components/Home/HomeSchemaOrg.jsx
@@ -1,30 +1,54 @@
 import { useIntl } from 'react-intl';
 import { toPublicURL } from '@plone/volto/helpers';
+import { useSelector } from 'react-redux';
 import { SiteProperty } from 'volto-site-settings';
 import { SchemaOrg, getSiteProperty } from 'io-sanita-theme/helpers';
 
 const HomeSchemaOrg = ({ content }) => {
   const intl = useIntl();
+  const socialSettings = useSelector((state) => state.socialSettings?.results);
+
+  const name = SiteProperty({
+    property: 'site_title',
+    defaultValue: getSiteProperty('siteTitle', intl.locale),
+    getValue: true,
+  });
+  const description = SiteProperty({
+    property: 'site_subtitle',
+    defaultValue: getSiteProperty('siteSubtitle', intl.locale),
+    getValue: true,
+  });
+
   let schemaOrg = null;
   if (content?.['@type'] == 'Plone Site' || content?.['@type'] == 'LRF') {
-    const name = SiteProperty({
-      property: 'site_title',
-      defaultValue: getSiteProperty('siteTitle', intl.locale),
-      getValue: true,
-    });
-    const description = SiteProperty({
-      property: 'site_subtitle',
-      defaultValue: getSiteProperty('siteSubtitle', intl.locale),
-      getValue: true,
-    });
+    let socialLinks = null;
+    if (socialSettings && socialSettings?.length > 0) {
+      socialLinks = socialSettings.map((item) => item.url);
+    }
 
     schemaOrg = {
-      '@type': 'MedicalOrganization',
-      name: name,
-      description: description,
-      url: toPublicURL(content['@id']),
+      '@graph': [
+        {
+          '@type': 'WebSite',
+          url: toPublicURL('/'),
+          name: name,
+          description: description || null,
+          potentialAction: {
+            '@type': 'SearchAction',
+            target: toPublicURL(content['@id']) + '/search',
+          },
+        },
+        {
+          '@type': 'MedicalOrganization',
+          name: name,
+          description: description || null,
+          url: toPublicURL('/'),
+          sameAs: socialLinks,
+        },
+      ],
     };
   }
+
   return schemaOrg ? <SchemaOrg schema={schemaOrg} /> : <></>;
 };
 
