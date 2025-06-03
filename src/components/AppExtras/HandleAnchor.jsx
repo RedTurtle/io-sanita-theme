811d25edfcad740736a6c6b817b8de273d9d237f
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-06-03 16:16:06 +0200

    fix: handle anchor scroll

--- src/components/AppExtras/HandleAnchor.jsx
+++ src/components/AppExtras/HandleAnchor.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { scrollIntoView, getMainOffset } from 'io-sanita-theme/helpers';
 
 const HandleAnchor = () => {
   const location = useLocation();
   useEffect(() => {
     if (location.hash) {
       // eslint-disable-next-line no-unused-expressions
-      document.getElementById(location.hash.replace('#', ''))?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'center',
-      });
+      const _id = location.hash.replace('#', '');
+      const ref = document.querySelector('#' + _id);
+      ref.focus();
+
+      setTimeout(() => {
+        scrollIntoView({ ref });
+      }, 30);
     }
   }, [location]);
   return <></>;
