6eb5a1e271e7e303aa228046a36f2d3e23e270f9
Sara Bianchi <sonia_sara@live.it>
2025-06-12 14:16:55 +0200

    chore: updated StrutturaPersonale with backreferences

--- src/components/View/Struttura/StrutturaPersonale.jsx
+++ src/components/View/Struttura/StrutturaPersonale.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-import { RichTextSection } from 'io-sanita-theme/helpers';
-import { CardPersona } from 'io-sanita-theme/components';
-import { Row, Col } from 'design-react-kit';
+import { BackReferences } from 'io-sanita-theme/components/View/commons';
 
 const messages = defineMessages({
   personale: {
@@ -14,22 +12,13 @@ const messages = defineMessages({
 const StrutturaPersonale = ({ content }) => {
   const intl = useIntl();
 
-  return content?.personale_struttura?.length > 0 ? (
-    <RichTextSection
-      tag_id="personale"
+  return (
+    <BackReferences
+      type="Persona"
+      content={content}
+      id={'Persona'}
       title={intl.formatMessage(messages.personale)}
-    >
-      {/* PERSONALE DELLA STRUTTURA */}
-      <Row className="mb-5">
-        {content.personale_struttura.map((item) => (
-          <Col lg={6} className="py-lg-2" key={'personale_' + item['@id']}>
-            <CardPersona item={item} />
-          </Col>
-        ))}
-      </Row>
-    </RichTextSection>
-  ) : (
-    <></>
+    />
   );
 };
 
