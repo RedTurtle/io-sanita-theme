59b05aeed581317fdf256c8ab8dc4f2cc69303ad
Sara Bianchi <sonia_sara@live.it>
2025-06-17 12:23:57 +0200

    chore: restored personale_correlato

--- src/components/View/Struttura/StrutturaPersonale.jsx
+++ src/components/View/Struttura/StrutturaPersonale.jsx
@@ -10,11 +10,16 @@ const messages = defineMessages({
     id: 'struttura_personale',
     defaultMessage: 'Personale della struttura',
   },
+  personale_correlato: {
+    id: 'struttura_personale_correlato',
+    defaultMessage: 'Personale correlato',
+  },
 });
 
 const StrutturaPersonale = ({ content }) => {
   const intl = useIntl();
 
+<<<<<<< Updated upstream
   return content?.personale_struttura?.length > 0 ? (
     <RichTextSection
       tag_id="personale"
@@ -36,6 +41,50 @@ const StrutturaPersonale = ({ content }) => {
       id="Persona"
       title={intl.formatMessage(messages.personale)}
     />
+=======
+  return (
+    <>
+      {/* PERSONALE DELLA STRUTTURA */}
+      {content?.personale_struttura?.length > 0 ? (
+        <RichTextSection
+          tag_id="personale"
+          title={intl.formatMessage(messages.personale)}
+        >
+          {/* PERSONALE DELLA STRUTTURA - campo manuale, se compilato vince sopra alle backreference */}
+          <Row className="mb-5">
+            {content.personale_struttura.map((item) => (
+              <Col lg={6} className="py-lg-2" key={'personale_' + item['@id']}>
+                <CardPersona item={item} />
+              </Col>
+            ))}
+          </Row>
+        </RichTextSection>
+      ) : (
+        <BackReferences
+          type="Persona"
+          content={content}
+          id={'Persona'}
+          title={intl.formatMessage(messages.personale)}
+        />
+      )}
+
+      {/* PERSONALE CORRELATO */}
+      {content?.personale_correlato?.length > 0 && (
+        <RichTextSection
+          tag_id="personale_correlato"
+          title={intl.formatMessage(messages.personale_correlato)}
+        >
+          <Row className="mb-5">
+            {content.personale_correlato.map((item) => (
+              <Col lg={6} className="py-lg-2" key={'personale_' + item['@id']}>
+                <CardPersona item={item} />
+              </Col>
+            ))}
+          </Row>
+        </RichTextSection>
+      )}
+    </>
+>>>>>>> Stashed changes
   );
 };
 
