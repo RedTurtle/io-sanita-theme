3357e516708c658dee54a75fbc1f5dd85e84b410
Sara Bianchi <43245702+SaraBianchi@users.noreply.github.com>
2025-06-17 17:38:33 +0200

    chore: updated Personale field ct struttura with backreferences and personale_correlato (#95)
    * chore: updated Struttura ct with personale field
    
    * chore: updated StrutturaPersonale with backreferences
    
    * chore: updated personale field of Struttura CT
    
    * Update src/components/View/Struttura/StrutturaPersonale.jsx
    
    * chore: restored personale_correlato
    
    * fix: restored personale_correlato
    
    ---------
    
    Co-authored-by: Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>

--- src/components/View/Struttura/StrutturaPersonale.jsx
+++ src/components/View/Struttura/StrutturaPersonale.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
+import { RichTextSection } from 'io-sanita-theme/helpers';
+import { CardPersona } from 'io-sanita-theme/components';
+import { Row, Col } from 'design-react-kit';
 import { BackReferences } from 'io-sanita-theme/components/View/commons';
 
 const messages = defineMessages({
@@ -12,11 +15,25 @@ const messages = defineMessages({
 const StrutturaPersonale = ({ content }) => {
   const intl = useIntl();
 
-  return (
+  return content?.personale_correlato?.length > 0 ? (
+    <RichTextSection
+      tag_id="personale"
+      title={intl.formatMessage(messages.personale)}
+    >
+      {/* PERSONALE DELLA STRUTTURA - campo manuale, se compilato vince sopra ai correlati */}
+      <Row className="mb-5">
+        {content.personale_correlato.map((item) => (
+          <Col lg={6} className="py-lg-2" key={'personale_' + item['@id']}>
+            <CardPersona item={item} />
+          </Col>
+        ))}
+      </Row>
+    </RichTextSection>
+  ) : (
     <BackReferences
       type="Persona"
       content={content}
-      id={'Persona'}
+      id="Persona"
       title={intl.formatMessage(messages.personale)}
     />
   );
