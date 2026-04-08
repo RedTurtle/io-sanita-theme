acf6304bf0be2ff4cab051dd5c63f3967b3ed968
giulio-red-turtle <giulio.malaguti@redturtle.it>
2026-04-08 09:55:56 +0200

    fix: restored code

--- src/config/blocks/index.js
+++ src/config/blocks/index.js
@@ -290,6 +290,10 @@ export const applyIoSanitaBlocksConfig = (config) => {
     },
     search: {
       ...config.blocks.blocksConfig.search,
+      // filtro top non personalizzato / non funzionante con layout agid
+      variations: config.blocks.blocksConfig.search.variations.filter(
+        (v) => v.id !== 'facetsTopSide',
+      ),
       templates: [
         ...listingVariations.map((v) => v.id).filter((v) => v !== 'carousel'),
       ],
