4620fe0185d8845da693207e6ce4476a3656fc8a
giulio-red-turtle <giulio.malaguti@redturtle.it>
2026-04-13 10:03:48 +0200

    fix: message servizio disponibile e prenotabile removed

--- src/components/View/commons/PageHeader/PageHeaderStatoServizio.jsx
+++ src/components/View/commons/PageHeader/PageHeaderStatoServizio.jsx
@@ -1,25 +1,8 @@
-import { defineMessages, useIntl } from 'react-intl';
 import { BadgeStatusServizio } from 'io-sanita-theme/components/View/Servizio';
 
-const messages = defineMessages({
-  service_available: {
-    id: 'Service available',
-    defaultMessage: 'Servizio disponibile e prenotabile',
-  },
-});
-
 const PageHeaderServizio = ({ content }) => {
-  const intl = useIntl();
-
   return content['@type'] === 'Servizio' ? (
-    <div className="mb-3">
-      <BadgeStatusServizio item={content} />
-      {content.servizio_attivo && (
-        <span className="px-2">
-          {intl.formatMessage(messages.service_available)}
-        </span>
-      )}
-    </div>
+    <BadgeStatusServizio item={content} wrapperClassName="mb-3" />
   ) : null;
 };
 
