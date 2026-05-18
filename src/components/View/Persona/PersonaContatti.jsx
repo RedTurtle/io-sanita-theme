6329c05bc83b416c44856285f8c7a0c80d6a600a
Ilaria Massa <ilaria@Mac.homenet.telecomitalia.it>
2026-05-18 15:52:31 +0200

    feat: added text block in contact section inside CT Person to add contact info

--- src/components/View/Persona/PersonaContatti.jsx
+++ src/components/View/Persona/PersonaContatti.jsx
@@ -9,26 +9,45 @@ const messages = defineMessages({
     id: 'persona_contatti',
     defaultMessage: 'Contatti',
   },
+  informazioni_contatto: {
+    id: 'persona_informazioni_contatto',
+    defaultMessage: 'Informazioni di contatto',
+  },
 });
 
 const PersonaContatti = ({ content }) => {
   const intl = useIntl();
 
-  return content?.pdc_correlato?.length > 0 ? (
-    <RichTextSection
-      tag_id="contatti"
-      title={intl.formatMessage(messages.contatti)}
-    >
-      <Row>
-        {/* Punto di contatto */}
-        {content?.pdc_correlato.map((item, i) => (
-          <Col lg={6} className="py-lg-2" key={item['@id']}>
-            <CardContatti item={item} show_title={true} />
-          </Col>
-        ))}
-      </Row>
-    </RichTextSection>
-  ) : null;
+  return content?.pdc_correlato?.length > 0 ||
+    content?.informazioni_contatto?.data ?
+    (
+      <RichTextSection
+        tag_id="contatti"
+        title={intl.formatMessage(messages.contatti)}
+      >
+        <Row>
+          {/* Punto di contatto */}
+          {content?.pdc_correlato.map((item, i) => (
+            <Col lg={6} className="py-lg-2" key={item['@id']}>
+              <CardContatti item={item} show_title={true} />
+            </Col>
+          ))}
+        </Row>
+        <Row>
+          {/* Informazioni di contatto */}
+          {content?.informazioni_contatto?.data && (
+            <Col lg={6} className="py-lg-2 informazioni-contatto-wrapper">
+              <RichTextSection
+                data={content.informazioni_contatto}
+                tag_id="informazioni_contatto"
+                title={intl.formatMessage(messages.informazioni_contatto)}
+              />
+            </Col>
+          )}
+        </Row>
+      </RichTextSection>
+
+    ) : null;
 };
 
 PersonaContatti.propTypes = {
