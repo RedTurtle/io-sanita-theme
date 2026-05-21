8d261e8c027e284b4c060a30391dcc769af3d23d
ilariamassa96 <ilaria.massa@redturtle.it>
2026-05-21 09:28:28 +0200

    feat: added text block in contact section inside CT Person to add contact info (#140)
    * feat: added text block in contact section inside CT Person to add contact info
    
    * refactored code
    
    ---------
    
    Co-authored-by: Ilaria Massa <ilaria@Mac.homenet.telecomitalia.it>
    Co-authored-by: Ilaria Massa <ilaria@MacBook-Pro-di-Redturtle.local>

--- src/components/View/Persona/PersonaContatti.jsx
+++ src/components/View/Persona/PersonaContatti.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { defineMessages, useIntl } from 'react-intl';
 import { CardContatti } from 'io-sanita-theme/components';
-import { RichTextSection } from 'io-sanita-theme/helpers';
+import { RichTextSection, richTextHasContent } from 'io-sanita-theme/helpers';
 import { Row, Col } from 'design-react-kit';
 
 const messages = defineMessages({
@@ -9,26 +9,45 @@ const messages = defineMessages({
     id: 'persona_contatti',
     defaultMessage: 'Contatti',
   },
+  informazioni_contatto: {
+    id: 'persona_informazioni_contatto',
+    defaultMessage: 'Informazioni di contatto',
+  },
 });
 
 const PersonaContatti = ({ content }) => {
   const intl = useIntl();
 
-  return content?.pdc_correlato?.length > 0 ? (
-    <RichTextSection
-      tag_id="contatti"
-      title={intl.formatMessage(messages.contatti)}
-    >
-      <Row>
-        {/* Punto di contatto */}
-        {content?.pdc_correlato.map((item, i) => (
-          <Col lg={6} className="py-lg-2" key={item['@id']}>
-            <CardContatti item={item} show_title={true} />
-          </Col>
-        ))}
-      </Row>
-    </RichTextSection>
-  ) : null;
+  return content?.pdc_correlato?.length > 0 ||
+    richTextHasContent(content?.informazioni_contatto) ?
+    (
+      <RichTextSection
+        tag_id="contatti"
+        title={intl.formatMessage(messages.contatti)}
+      >
+        <Row>
+          {/* Punto di contatto */}
+          {content?.pdc_correlato.map((item, i) => (
+            <Col lg={6} className="py-lg-2" key={item['@id']}>
+              <CardContatti item={item} show_title={true} />
+            </Col>
+          ))}
+        </Row>
+        <Row>
+          {/* Informazioni di contatto */}
+          {richTextHasContent(content?.informazioni_contatto) && (
+            <Col lg={6} className="py-lg-2 informazioni-contatto-wrapper">
+              <RichTextSection
+                data={content.informazioni_contatto}
+                tag_id="informazioni_contatto"
+                title={intl.formatMessage(messages.informazioni_contatto)}
+              />
+            </Col>
+          )}
+        </Row>
+      </RichTextSection>
+
+    ) : null;
 };
 
 PersonaContatti.propTypes = {
