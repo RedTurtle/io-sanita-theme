4ff54b01727f1c0455a2cd8b04aa50418e010f10
Mauro Amico <mauro.amico@gmail.com>
2026-05-27 15:08:01 +0200

    feat: ulteriori info in uo (#144)

--- src/components/View/UOView/UOUlterioriInformazioni.jsx
+++ src/components/View/UOView/UOUlterioriInformazioni.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { richTextHasContent, RichTextSection } from 'io-sanita-theme/helpers';
 import { defineMessages, useIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
+import { RichTextSection } from 'io-sanita-theme/helpers';
 
 const messages = defineMessages({
   other_info: {
     id: 'other_info',
     defaultMessage: 'Ulteriori informazioni',
   },
+    come_accedere: {
+    id: 'struttura_come_accedere',
+    defaultMessage: 'Come accedere',
+  },
 });
 
 const UOUlterioriInformazioni = ({ content }) => {
   const intl = useIntl();
-  return richTextHasContent(content.ulteriori_informazioni) ? (
+  const has_richTextUlteriori = richTextHasContent(content?.ulteriori_informazioni);
+  const has_come_accedere = richTextHasContent(content?.come_accedere);
+  return (has_richTextUlteriori || has_come_accedere) ? (
     <RichTextSection
-      data={content.ulteriori_informazioni}
       tag_id="more_info"
       title={intl.formatMessage(messages.other_info)}
       anchorOffset={true}
-    />
+    >
+      {content.ulteriori_informazioni && (
+        <RichText data={content.ulteriori_informazioni} />
+      )}
+      {content.come_accedere && (
+        <div className="mt-4">
+          <h3>{intl.formatMessage(messages.come_accedere)}</h3>
+          <RichText data={content.come_accedere} />
+        </div>
+      )}
+    </RichTextSection>
   ) : null;
 };
 
