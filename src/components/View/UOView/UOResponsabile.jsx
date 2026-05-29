c6c81a943ed60b3b25bc0dd46b5af8e5c2268f81
Mauro Amico <mauro.amico@gmail.com>
2026-05-29 17:30:55 +0200

    format

--- src/components/View/UOView/UOResponsabile.jsx
+++ src/components/View/UOView/UOResponsabile.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-import { RichTextSection, richTextHasContent, RichText } from 'io-sanita-theme/helpers';
+import {
+  RichTextSection,
+  richTextHasContent,
+  RichText,
+} from 'io-sanita-theme/helpers';
 import { CardPersona } from 'io-sanita-theme/components';
 import { Row, Col } from 'design-react-kit';
 
@@ -17,8 +21,7 @@ const UOResponsabile = ({ content }) => {
     content?.responsabile_correlato_text,
   );
 
-<<<<<<< lint
-  return content?.responsabile_correlato?.length > 0 ? (
+  return content?.responsabile_correlato?.length > 0 || has_richTextContent ? (
     <RichTextSection
       tag_id="responsabile"
       title={intl.formatMessage(messages.responsabile)}
@@ -32,33 +35,16 @@ const UOResponsabile = ({ content }) => {
           );
         })}
       </Row>
-=======
-  return content?.responsabile_correlato?.length > 0 || has_richTextContent ? (
-      <RichTextSection
-        tag_id="responsabile"
-        title={intl.formatMessage(messages.responsabile)}
-      >
-        <Row>
-          {content.responsabile_correlato.map((item) => {
-            return (
-              <Col lg={6} className="py-lg-2" key={'responsabile_' + item['@id']}>
-                <CardPersona item={item} />
-              </Col>
-            );
-          })}
-        </Row>
       {/* Contenuto testuale alternativo, usato ad esempio con il sync virtualdesk */}
       {has_richTextContent && (
         <div className="richtext-blocks font-serif">
-            <RichText data={content?.responsabile_correlato_text} />
+          <RichText data={content?.responsabile_correlato_text} />
         </div>
       )}
->>>>>>> main
     </RichTextSection>
   ) : (
     <></>
   );
-
 };
 
 export default UOResponsabile;
